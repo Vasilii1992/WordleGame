dfca0a2a51509c01e3e9e329fd6aad1cd46732f5
Vasilii Tikhonov <tihonovvasilij154@gmail.com>
2024-09-08 16:38:01 +0300

    added the ability to work with an ipad to the alert

--- Sources/WordleGame/WordleViewController.swift
+++ Sources/WordleGame/WordleViewController.swift
@@ -119,6 +119,11 @@ extension WordleViewController: KeyboardViewControllerDelegate {
         ac.addAction(UIAlertAction(title: "Reset", style: .default, handler: { [weak self] _ in
             self?.resetGame()
         }))
+        if let popoverController = ac.popoverPresentationController {
+            popoverController.sourceView = self.view
+            popoverController.sourceRect = CGRect(x: self.view.bounds.midX, y: self.view.bounds.midY, width: 0, height: 0)
+            popoverController.permittedArrowDirections = []
+        }
         present(ac, animated: true)
     }
 }
